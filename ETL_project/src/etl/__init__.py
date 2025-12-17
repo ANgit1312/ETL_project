73b8547775c0d86104fd0ab5a37ec5bd23463238
Ankit Kumar Mittal <125180309+ANgit1312@users.noreply.github.com>
2025-12-18 05:19:47 +0530

    Update __init__.py

--- ETL_project/src/etl/__init__.py
+++ ETL_project/src/etl/__init__.py
@@ -1,13 +1,1 @@
-# src/etl/__init__.py
 
-from .ingest import ingest_csv, ingest_api, ingest_stream
-from .transform import clean_data
-from .load import load_to_db
-
-__all__ = [
-    "ingest_csv",
-    "ingest_api",
-    "ingest_stream",
-    "clean_data",
-    "load_to_db",
-]
