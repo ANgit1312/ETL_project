5221227f548f228413b69ac96fadd88232f4e0b3
Ankit Kumar Mittal <125180309+ANgit1312@users.noreply.github.com>
2025-12-18 05:21:02 +0530

    Update config.py

--- ETL_project/src/etl/config.py
+++ ETL_project/src/etl/config.py
@@ -1,11 +1,3 @@
-import os
-from dotenv import load_dotenv
-
-# Load environment variables (if .env file exists)
-load_dotenv()
-
-DB_URI = os.getenv("DB_URI", "postgresql://etl_user:etl_pass@localhost:5432/etl_db")
-
-# Data sources
-CSV_PATH = os.getenv("CSV_PATH", "data/sales.csv")
-API_URL = os.getenv("API_URL", "https://fakestoreapi.com/products")
+DB_URI = "postgresql://etl_user:etl_pass@localhost:5432/etl_db"
+CSV_PATH = "data/sales.csv"
+API_URL = "https://fakestoreapi.com/products"
